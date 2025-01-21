81758b8b5f1d39aa6f854ac61e37df4076667096
Mazur Oleh <mazuroleg75@gmail.com>
2025-01-21 13:15:42 +0200

    feature: +auth middleware

--- src/routes/participants.ts
+++ src/routes/participants.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { registerParticipant, getUserEvents } from "../controllers/participantController";
+import { authenticate } from "../middleware/auth";
 
 const router = Router();
 
-router.post("/", registerParticipant);
+router.post("/", authenticate, registerParticipant);
 router.get("/:userId/events", getUserEvents);
 
 export default router;
